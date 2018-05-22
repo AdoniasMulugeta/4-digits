6df3fce3a73f9ae532c6014ee5f62e3449441eb7
Adonias Mulugeta <amulugeta@kukulugame.com>
2018-05-22 18:39:56 +0300

    Implemented the randomValueGenerator.cs for only numbers

---
+++ Assets/Scripts/DAO/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+	public int digitsSize;
+	public int maxTries;
+
+	struct digitsType{
+		static string NUMBER = "Number";
+		static string ALPHABET = "Alphabet";
+	}
+
+}
---
+++ Assets/Scripts/Logic/RandomValueGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomValueGenerator : MonoBehaviour {
+
+    void Print(List<int> list) {
+		print(list[0] +" "+ list[1] + " " + list[2] + " " + list[3]);
+    }
+	void Update(){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Print(generateNumbers(4, 0, 9));
+        }
+	}
+    List<int> generateNumbers(int length, int min, int max) {
+        List<int> usedNumbers = new List<int>();
+        List<int> generatedNumbers = new List<int>();
+        max++;//Because Random.Range max is exclusive
+        for (int i = 0; i < length; i++) {
+            int number = Random.Range(min, max);
+            while (usedNumbers.Contains(number))
+            {
+                number = Random.Range(min, max);
+            }
+            generatedNumbers.Add(number);
+			usedNumbers.Add (number);
+        }
+        return generatedNumbers;
+    }
+}
